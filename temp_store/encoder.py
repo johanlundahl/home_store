23df3aa037edc664ce93be71e8e21e7f2abaa95b
Lundahl <lundahl@Lundahls-MacBook-Air.local>
2019-09-05 18:38:58 +0200

    added handling of datetime

--- temp_store/encoder.py
+++ temp_store/encoder.py
@@ -1,9 +1,12 @@
 from flask.json import JSONEncoder
 from temp_store.model.sensor import Sensor
+from datetime import datetime
 
 class Encoder(JSONEncoder):
     
     def default(self, o):
         if any(type(o) is x for x in [Sensor]):
             return o.to_json()
+        if type(o) is datetime:
+        	return o.strftime('%Y-%m-%d %H:%M:%S')    
         return JSONEncoder.default(self, o)
