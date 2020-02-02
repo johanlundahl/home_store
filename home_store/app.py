eafc5492ff1ebbef2604ebf12c01bd93dbe8cc33
Lundahl <lundahl@Lundahls-MacBook-Air.local>
2020-02-02 20:06:11 +0100

    added history endpoint

--- home_store/app.py
+++ home_store/app.py
@@ -77,20 +77,13 @@ def sensor_trend(name):
 @app.route('/api/sensors/<name>/history', methods=['GET'])
 def sensor_history(name):
     if request.method == 'GET':
-
-        print('TIMESTAMP:', Filter.args_matching(request.args, 'timestamp'))
-
-        date_args = Filter.args_matching(request.args, 'date')
-        date_args += Filter.args_matching(request.args, 'timestamp')
-        date_filters = [Filter.from_arg(date, request.args[date]) for date in date_args]
-
+        from_time = datetime.strptime(request.args['from'], '%Y-%m-%d %H:%M:%S') if 'from' in request.args else datetime.now()-datetime.timedelta(days=1)
+        to_time = datetime.strptime(request.args['to'], '%Y-%m-%d %H:%M:%S') if 'to' in request.args else datetime.now()
         resolution = int(request.args['resolution']) if 'resolution' in request.args else 20
-        sort = request.args['sort'] if 'sort' in request.args else 'desc'
 
         with db:
-            sensors = db.sensor(name, offset=offset, limit=limit, sort=sort)
-            for date_filter in date_filters:
-                sensors = [s for s in sensors if date_filter.evaluate(s)]
+            sensors = db.sensor_history(name, from_time=from_time, to_time=to_time)
+            sensors = sensors[0::int(len(sensors)/resolution)] if len(sensors) > resolution else sensors
             return jsonify(sensors)
         return 500
 
--- home_store/db.py
+++ home_store/db.py
@@ -43,9 +43,9 @@ class MyDB():
         order_by = self.get_sort_order(sort)
         return self.session.query(Sensor).order_by(order_by).filter(Sensor.name == name, extract('minute', Sensor.timestamp) == 0).limit(limit).all()
 
-    def sensor_history(self, name, limit=24, sort='desc'):
+    def sensor_history(self, name, from_time=datetime.now()-timedelta(days=1), to_time=datetime.now()):
         order_by = self.get_sort_order(sort)
-        return self.session.query(Sensor).order_by(order_by).filter(Sensor.name == name, extract('hour', Sensor.timestamp) == 12, extract('minute', Sensor.timestamp) == 0).limit(limit).all()
+        return self.session.query(Sensor).filter(Sensor.name == name, Sensor.timestamp > from_time, Sensor.timestamp < to_time).order_by(Sensor.timestamp.desc()).all()
 
     def get_sort_order(self, sorting):
         order_by = Sensor.timestamp.asc() if sorting == 'asc' else Sensor.timestamp.desc()
