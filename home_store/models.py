731026d5b368a8c95ca7673357bb872800227a28
Johan Lundahl <johan.t.lundahl@gmail.com>
2022-12-12 10:20:24 +0100

    added import for json module

--- home_store/models.py
+++ home_store/models.py
@@ -4,6 +4,7 @@ from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import extract, func
 from sqlalchemy_filters import apply_filters
 from flask.json.provider import JSONProvider
+import json
 
 
 class MyDB(SQLAlchemy):
